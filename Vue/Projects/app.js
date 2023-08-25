a6fb603923e41eb2acc2cf2eb754ca31d49a7b0a
Sagar Nandy <82097180+AbstractDil@users.noreply.github.com>
2023-08-25 13:43:32 +0000

    Vue JS First Commit

---
+++ Vue/Projects/app.js
@@ -0,0 +1,13 @@
+
+Vue.createApp({
+
+    data(){
+        return {
+           
+            title: "Vue 3",
+            description: "Vue 3 is the latest version of Vue",
+
+        }
+    }
+
+}).mount("#app");
