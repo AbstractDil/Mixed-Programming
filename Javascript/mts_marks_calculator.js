06600fe3c47855df6202d572ce85eba0bbc76b63
Sagar Nandy <82097180+AbstractDil@users.noreply.github.com>
2023-09-17 17:46:11 +0000

    updated ssc score card generating system

--- Javascript/mts_marks_calculator.js
+++ Javascript/mts_marks_calculator.js
@@ -62,13 +62,61 @@ for (var totalMarks = 0, s = 0; s < 2; s++) {
 }
 console.log(" %c \nTotal Marks in Session-1 : " + totalMarks, "background: #222; color: skyblue;");
 
-//console.log("%c Powered by : MathHub Combined (https://mathhub.nandysagar.in) ", "background: #222; color: #ca5565;font-size:14px;")
+
 
 
 // Calculate Session-2  Score 
 // Each Question Carries 3 marks and there is 1 negative marking for every wrong question
 
 
+for (var totalMarks_II = 0, s = 2; s < 4; s++) {
+  var sectionLabels = document.querySelectorAll('.section-lbl');
+  var sectionTexts = sectionLabels[s].querySelector('span.bold').textContent;
+  //console.log("Section Name : " + sectionTexts); 
+  for ( var right = 0,notAttempted = 0, bonus = 0, i = 25 * (s-2);i < 25 * (s-2) + 25;i++) {
+    //console.log(i);
+    " -- " ===
+    document.getElementsByClassName("grp-cntnr")[1].getElementsByClassName("question-pnl")[i].getElementsByClassName("bold")[9].textContent && notAttempted++;
+      
+    try {
+      document.getElementsByClassName("grp-cntnr")[1]
+        .getElementsByClassName("question-pnl")
+        [i].getElementsByClassName("rightAns")[0].textContent[0] ===
+        document.getElementsByClassName("grp-cntnr")[1]
+          .getElementsByClassName("question-pnl")
+          [i].getElementsByClassName("bold")[9].textContent && right++;
+    } catch {
+      bonus++;
+    }
+  }
+(wrong = 25 - notAttempted - right - bonus),
+  (marks = 3 * (right + bonus) - 1 * wrong),
+  (totalMarks_II += marks);
+
+console.log(
+  "%c" +
+  "Section Name : " +
+  sectionTexts +
+    "\nAttempted : " +
+    (25 - notAttempted) +
+    "\nRight Answers : " +
+    right +
+    "\nWrong Answers : " +
+    wrong +
+    "\nBonus : " +
+    bonus +
+    "\nMarks : " +
+    marks,  "background: #222; color:#D3D3D3;font-size:12px;"
+);
+}
+
+console.log(" %c \nTotal Marks in Session-II : " + totalMarks_II, "background: #222; color: skyblue;");
+
+
+console.log("%c SSC MTS 2023 Score Card Generating System \n Powered by : MathHub Combined (https://mathhub.nandysagar.in) \n Developed by : Sagar Nandy (https://www.nandysagar.in/) ", "background: #222; color: #ca5565;")
+
+
+/*
     // Score For GS Section
 
     for (var totalMarks_GS = 0, s = 2; s < 3; s++) {
@@ -168,4 +216,8 @@ console.log(" %c \nTotal Marks in Session-1 : " + totalMarks, "background: #222;
   const totalMarksII = totalMarks_GS + totalMarks_Eng;
   
   console.log(" %c \nTotal Marks in Session-II : " + totalMarksII, "background: #222; color: skyblue;");
-  console.log("%c SSC MTS 2023 Score Card Generating System \n Powered by : MathHub Combined (https://mathhub.nandysagar.in) \n Developed by : Sagar Nandy (https://www.nandysagar.in/) ", "background: #222; color: #ca5565;")
+  */
+
+
+
+  
