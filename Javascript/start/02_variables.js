40bbb32750877570cbb8c56bf7354b93968656bc
Sagar Nandy <82097180+AbstractDil@users.noreply.github.com>
2023-10-16 12:01:26 +0000

    updated

---
+++ Javascript/start/02_variables.js
@@ -0,0 +1,18 @@
+// declare variables in javascipt
+
+// to declare variable we use
+var x;
+// var x doesnot contain any value
+
+var x = 10; // value assign to variable 
+
+/*
+let : used to declare a variable that can be reassigned later
+const :  used to declare a variable that cannot be reassigned
+
+Example:
+
+*/
+
+let x=2;
+
