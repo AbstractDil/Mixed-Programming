0e063467edf0272b56039e43b73bea83dee79fab
Sagar Nandy <82097180+AbstractDil@users.noreply.github.com>
2024-10-26 17:58:32 +0000

    upto for loop

--- Vue/Projects/app.js
+++ Vue/Projects/app.js
@@ -8,7 +8,16 @@ Vue.createApp({
             description: "Vue 3 is the latest version of Vue",
             url: "https://nandysagar.in/",
             imgUrl : "https://plus.unsplash.com/premium_photo-1663100722417-6e36673fe0ed?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            imgTitle : "Img from Unsplash"
+            imgTitle : "Img from Unsplash",
+            items:2,
+            isStock: true,
+            showDiv:false,
+            fruits:['mango', 'banana', 'pineapple'],
+            manyFoods: [
+                {name: 'Burrito', id:1},
+                {name: 'Salad', id:2},
+                {name: 'Cake', id:3},
+              ]
 
         }
     }
